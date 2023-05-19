673891abcd03082efd14d62a716cd58e5353885c
Aissam Errahouti <milbournato@gmail.com>
2023-05-19 21:40:40 +0100

    Initial commit

---
+++ src/main/java/com/example/gestionDeCovoiturage/GestionDeCovoiturageApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionDeCovoiturage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeCovoiturageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeCovoiturageApplication.class, args);
+	}
+
+}
